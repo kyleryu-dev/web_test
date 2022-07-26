706dfa0e4db5c49e98c931de60e200815e2e8316
Kyle Ryu <kyleryu6698@gmail.com>
2022-07-26 19:20:50 +0900

    Apply the script

---
+++ script/script.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  /* scroll 함수 */
+  $(window).scroll(function() {
+    /* scroll_down 애니메이션 */
+    if($(window).scrollTop() > 10) {
+      $('.scroll_down').css('opacity',0);
+    } else {
+      $('.scroll_down').css('opacity',1);
+    }
+  });
+  /* //scroll 함수 - 하나만 사용 가능 => 이 함수 내부에 스크롤 기능 필요한 함수 모을 것 */
+    
+  
+});
+/* //ready function */
