ad6b955c5dfaff583a4b29976626bdb29bd83787
Kyle Ryu <kyleryu6698@gmail.com>
2022-07-28 00:52:17 +0900

    Apply media query to button

--- script/mq.js
+++ script/mq.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  var clone = $('#btn');
   
   /* Media Query for Desktop */
   const mediaSize1024 = window.matchMedia('(min-width: 1024px)');
@@ -6,9 +8,13 @@ $(document).ready(function() {
       if (e.matches) {
         $('#real_last').removeClass('full');
         $('#real_last').addClass('center');
+        $('#transformer').addClass('cover')
+        $('#dwl_2').append(clone);
       } else {
         $('#real_last').removeClass('center');
         $('#real_last').addClass('full');
+        $('#transformer').removeClass('cover')
+        $('#btn').remove();
       }
     }
   mediaSize1024.addListener(changeMedia);
